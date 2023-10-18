209f65b2969561485d83682bf771901eaf8906ac
Zafar17 <zafariqbal.zafi@gmail.com>
2023-10-18 23:16:41 +0500

    feat: use sendStream in handleCallAccepted

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -45,6 +45,7 @@ const RoomPage = () => {
     const handleCallAccepted = useCallback(async ({ from, ans }) => {
         peer.setLocalDescription(ans)
         console.log('CALL ACCEPTED!')
+        sendStream()
     }, [myStream])
 
 
