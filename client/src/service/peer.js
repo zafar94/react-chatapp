3e3300561327ed35b292fb3ec97760c653eacfe4
Zafar17 <zafariqbal.zafi@gmail.com>
2023-10-18 21:32:30 +0500

    feat: create peer service and and added iceservers

---
+++ client/src/service/peer.js
@@ -0,0 +1,16 @@
+class PeerService {
+    constructor() {
+        if (!this.peer) {
+            this.peer = new RTCPeerConnection({
+                iceServers: [
+                    {
+                        urls: [
+                            "stun:stun.l.google.com:19302",
+                            "stun:global.stun.twilio.com:3478",
+                        ]
+                    }
+                ]
+            })
+        }
+    }
+}
