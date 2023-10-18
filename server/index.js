a5b81a2fdfc70ea028c87c575eec08aed94b01a3
Zafar17 <zafariqbal.zafi@gmail.com>
2023-10-18 22:57:19 +0500

    feat: negotiation on socket server hanlded

--- server/index.js
+++ server/index.js
@@ -30,4 +30,8 @@ io.on("connection", (socket) => {
   socket.on('call:accepted', ({ to, ans }) => {
     io.to(to).emit('call:accepted', { from: socket.id, ans })
   })
+
+  socket.on('peer:nego:needed', ({ to, offer }) => {
+    io.to(to).emit('peer:nego:needed', { from: socket.id, offer })
+  })
 });
