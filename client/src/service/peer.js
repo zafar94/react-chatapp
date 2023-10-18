c2f49065bfae7fdf3e7c707d9a56ceecf80fce25
Zafar17 <zafariqbal.zafi@gmail.com>
2023-10-18 21:35:33 +0500

    feat: get offer and set into local

--- client/src/service/peer.js
+++ client/src/service/peer.js
@@ -13,4 +13,12 @@ class PeerService {
             })
         }
     }
+
+    async getOffer() {
+        if (this.peer) {
+            const offer = await this.peer.createOffer();
+            await this.peer.setLocalDescription(new RTCSessionDescription(offer));
+            return offer;
+        }
+    }
 }
