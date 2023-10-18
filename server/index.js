2749f53da0bd6990f890a2df91f0f73632278339
Zafar17 <zafariqbal.zafi@gmail.com>
2023-10-18 20:36:49 +0500

    feat: enable cors and create maps for both email to socket and socket to email and them emit them

--- server/index.js
+++ server/index.js
@@ -1,8 +1,26 @@
 const { Server } = require("socket.io");
 
-const io = new Server(8000);
+const io = new Server(8000, {
+  cors: true
+});
+
+const emailToSocketIdMap = new Map();
+const socketIdToEmailMap = new Map();
+
 
 io.on("connection", (socket) => {
   console.log(`Socket Connected`, socket.id);
+  socket.on('room:join', (data) => {
+    const { email, room } = data
+    console.log(`room:join data`, data);
+    emailToSocketIdMap.set(email, socket.id);
+    socketIdToEmailMap.set(socket.id, email)
+    io.to(room).emit('user:joined', {
+      email,
+      id: socket.id
+    })
+    socket.join(room)
+    io.to(socket.id).emit('room:join', data)
+  })
 
 });
