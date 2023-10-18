eec3dd73cfac90c23c9097bf65b316343f2fd51b
Zafar17 <zafariqbal.zafi@gmail.com>
2023-10-18 21:59:36 +0500

    feat: get answer for incoming call

--- client/src/service/peer.js
+++ client/src/service/peer.js
@@ -14,6 +14,15 @@ class PeerService {
         }
     }
 
+    async getAnswer(offer) {
+        if (this.peer) {
+            await this.peer.setRemoteDescription(offer);
+            const ans = await this.peer.createAnswer();
+            await this.peer.setLocalDescription(new RTCSessionDescription(ans));
+            return ans;
+        }
+    }
+
     async getOffer() {
         if (this.peer) {
             const offer = await this.peer.createOffer();
